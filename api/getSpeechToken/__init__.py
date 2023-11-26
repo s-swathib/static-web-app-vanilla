c6b5d256ff6b7895fb8873a6213fdd932804fc14
Swathi S <105411200+s-swathib@users.noreply.github.com>
2023-11-26 18:02:36 +0530

    Update __init__.py

--- api/getSpeechToken/__init__.py
+++ api/getSpeechToken/__init__.py
@@ -1,34 +1,33 @@
 import logging
 import requests
 import os
-
-import azure.functions as func
-
+import sys
+ 
 # Define subscription key and region
-subscription_key = os.getenv("AZURE_SPEECH_API_KEY")
-region = os.getenv("AZURE_SPEECH_REGION")
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
+subscription_key = "f22920f0f7d64ce39ec6aa9ab6ca06a1"
+region = "westus2"
+ 
+def get_access_token():
     # Define token endpoint
     token_endpoint = f"https://{region}.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-    headers = {
-        "Ocp-Apim-Subscription-Key": subscription_key
-        "Access-Control-Allow-Methods": "POST"
-    
-    }
-    
-    #response = requests.get(bl_url, headers=headers)
-
+ 
     # Make HTTP request with subscription key as header
-    response = requests.post(token_endpoint, headers=headers)
-
+    response = requests.post(token_endpoint, headers={"Ocp-Apim-Subscription-Key": subscription_key})
+ 
     if response.status_code == 200:
-        access_token = response.text
-        return func.HttpResponse(
-             access_token,
-             status_code=200
-        )
+        return response.text
     else:
-        return func.HttpResponse(response.status_code)
+        return None
+ 
+def main():
+    logging.info('Processing a request.')
+ 
+    access_token = get_access_token()
+ 
+    if access_token:
+        return access_token
+    else:
+        logging.error("Failed to retrieve access token.")
+ 
+if __name__ == "__main__":
+    main()
