aada823d794441536fc382efcc0feb9d0d0e8db5
Swathi S <105411200+s-swathib@users.noreply.github.com>
2023-12-06 17:41:28 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -12,14 +12,24 @@ var system_prompt = `You are an AI assistant focused on delivering brief product
 - Pay attention to the language the customer is using in their latest statement and respond in the same language!
 `
 
-const TTSVoice = "en-US-JennyMultilingualNeural" // Update this value if you want to use a different voice
-const url = 'https://westus2.api.cognitive.microsoft.com/sts/v1.0/issueToken';
+// Logger
+const log = msg => {
+  document.getElementById('logging').innerHTML += msg + '<br>'
+}
 
+const ttsVoice = "en-US-JennyMultilingualNeural" // Update this value if you want to use a different voice
 const CogSvcRegion = "westus2" // Fill your Azure cognitive services region here, e.g. westus2
+const IceServerUrl = "turn:relay.communication.microsoft.com:3478"
+const IceServerUsername= "BQAANmXAyIAB2iE0CgIjuChTUuN6ju7NH2owrtXiS1AAAAAMARBLzcgb+8ZGv7VTu51ROGIsrn3j1xkOsVZBYYwYaz6M5IQwJe4="
+const IceServerCredential ="33qDidv0KCP3VDTvpWZCeSaDq2Y="
 
-const IceServerUrl = "turn:relay.communication.microsoft.com:3478" // Fill your ICE server URL here, e.g. turn:turn.azure.com:3478
-let IceServerUsername
-let IceServerCredential
+const cogSvcSubKey = "f22920f0f7d64ce39ec6aa9ab6ca06a1"
+const azureOpenAIEndpoint ="https://ash-aiml-workspace-aoai.openai.azure.com"
+const azureOpenAIApiKey = "2c0b6697ce4e4f9bacc62c224288594e"
+const azureOpenAIDeploymentName ="gpt-35-turbo-16k"
+const azureCogSearchEndpoint = "https://static-webapp-avatar.search.windows.net"
+const azureCogSearchApiKey = "GQemWa1Jk2KSO89crwqYbiaHFVh1OqVhMhrfYGvAfdAzSeCy37rz"
+const azureCogSearchIndexName = "products"
 
 // This is the only avatar which supports live streaming so far, please don't modify
 const TalkingAvatarCharacter = "lisa"
@@ -34,38 +44,30 @@ let token
 const speechSynthesisConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL("wss://{region}.tts.speech.microsoft.com/cognitiveservices/websocket/v1?enableTalkingAvatar=true".replace("{region}", CogSvcRegion)))
 
 // Global objects
+var speechRecognizer
 var speechSynthesizer
 var peerConnection
 var previousAnimationFrameTimestamp = 0
+var messages =[]
+var dataSources = []
+var avatarSynthesizer
+var speakingThreads = 0
 
-messages = [{ "role": "system", "content": system_prompt }];
+var enableQuickReply = false
+var quickReplies = [ 'Let me take a look.', 'Let me check.', 'One moment, please.' ]
 
-function removeDocumentReferences(str) {
-  // Regular expression to match [docX]
-  var regex = /\[doc\d+\]/g;
 
-  // Replace document references with an empty string
-  var result = str.replace(regex, '');
-
-  return result;
-}
+//messages = [{ "role": "system", "content": system_prompt }];
 
 // Setup WebRTC
-function setupWebRTC() {
+function setupWebRTC(IceServerUrl, IceServerUsername, IceServerCredential) {
   // Create WebRTC peer connection
-  //fetch("/api/getIceServerToken/", {
-    //method: 'POST'
-  //})
-    //.then(response => response.json())
-    //.then(response => { 
-  IceServerUsername = "BQAANmXAyIAB2iE0CgIjuChTUuN6ju7NH2owrtXiS1AAAAAMARBLzcgb+8ZGv7VTu51ROGIsrn3j1xkOsVZBYYwYaz6M5IQwJe4="
-  IceServerCredential = "33qDidv0KCP3VDTvpWZCeSaDq2Y="
-
   peerConnection = new RTCPeerConnection({
     iceServers: [{
-      urls: [IceServerUrl],
+      urls: [ IceServerUrl ],
       username: IceServerUsername,
-      credential: IceServerCredential}]
+      credential: IceServerCredential
+    }]
   })
   
   // Fetch WebRTC video stream and mount it to an HTML video element
@@ -78,6 +80,7 @@ function setupWebRTC() {
         remoteVideoDiv.removeChild(remoteVideoDiv.childNodes[i])
       }
     }
+    
     const videoElement = document.createElement(event.track.kind)
     videoElement.id = event.track.kind
     videoElement.srcObject = event.streams[0]
@@ -88,11 +91,10 @@ function setupWebRTC() {
     canvas = document.getElementById('canvas')
     remoteVideoDiv.hidden = true
     canvas.hidden = false
-
     videoElement.addEventListener('play', () => {
       remoteVideoDiv.style.width = videoElement.videoWidth / 2 + 'px'
       window.requestAnimationFrame(makeBackgroundTransparent)
-   })
+    })
   }
     
   // Make necessary update to the web page when the connection state changes
@@ -105,7 +107,7 @@ function setupWebRTC() {
     }
     
     if (peerConnection.iceConnectionState === 'disconnected') {
-    }
+      }
   }
     
   // Offer to receive 1 audio, and 1 video track
@@ -116,36 +118,93 @@ function setupWebRTC() {
   peerConnection.createOffer().then(sdp => {
     peerConnection.setLocalDescription(sdp).then(() => { setTimeout(() => { connectToAvatarService() }, 1000) })
   }).catch(console.log)
-    
-}  
+}
 
+// Initialize messages
+function initMessages() {
+  messages = []
 
-async function generateText(prompt) {
+  if (dataSources.length === 0) {
+      let systemMessage = {
+          role: 'system',
+          content: system_prompt
+      }
 
-  messages.push({
-    role: 'user',
-    content: prompt
-  });
-
-  let generatedText
-  let products
-  await fetch('/api/message', { 
-    method: 'GET', 
-    headers: { 
-      'Content-Type': 'application/json'}, 
-      body: JSON.stringify(messages) })
-  .then(response => response.json())
-  .then(data => {
-    generatedText = data["messages"][data["messages"].length - 1].content;
-    messages = data["messages"];
-    products = data["products"]
-  });
-
-  addToConversationHistory(generatedText, 'light');
-  if(products.length > 0) {
-    addProductToChatHistory(products[0]);
+      messages.push(systemMessage)
   }
-  return generatedText;
+}
+
+// Set data sources for chat API
+function setDataSources(azureCogSearchEndpoint, azureCogSearchApiKey, azureCogSearchIndexName) {
+  let dataSource = {
+      type: 'AzureCognitiveSearch',
+      parameters: {
+          endpoint: azureCogSearchEndpoint,
+          key: azureCogSearchApiKey,
+          indexName: azureCogSearchIndexName,
+          semanticConfiguration: '',
+          queryType: 'simple',
+          fieldsMapping: {
+              contentFieldsSeparator: '\n',
+              //contentFields: ['content'],
+              filepathField: null,
+              titleField: 'title',
+              urlField: null
+          },
+          inScope: true,
+          roleInformation: document.getElementById('prompt').value
+      }
+  }
+
+  dataSources.push(dataSource)
+}
+
+// Do HTML encoding on given text
+function htmlEncode(text) {
+  const entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;'
+  };
+
+  return String(text).replace(/[&<>"'\/]/g, (match) => entityMap[match])
+}
+
+// Speak the given text
+window.speak = (text) => {
+  async function speak(text) {
+    addToConversationHistory(text, 'dark')
+
+    // If there is any speaking thread, stop it
+    if (speakingThreads > 0) {
+      stopSpeaking()
+    }
+
+    speakingThreads++
+    let ssml = `<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='http://www.w3.org/2001/mstts' xml:lang='en-US'><voice name='${ttsVoice}'><mstts:leadingsilence-exact value='0'/>${htmlEncode(text)}</voice></speak>`
+    if (endingSilenceMs > 0) {
+        ssml = `<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='http://www.w3.org/2001/mstts' xml:lang='en-US'><voice name='${ttsVoice}'><mstts:leadingsilence-exact value='0'/>${htmlEncode(text)}<break time='${endingSilenceMs}ms' /></voice></speak>`
+    }
+    avatarSynthesizer.speakSsmlAsync(ssml).then((result) => {
+      speakingThreads--
+      if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
+        console.log(`Speech synthesized to speaker for text [ ${text} ]`)
+      } else {
+        console.log(`Error occurred while speaking the SSML.`)
+      }
+    }).catch(
+      (error) => {
+        console.log(`Error occurred while speaking the SSML: [ ${error} ]`)
+      })
+    }
+  speak(text);
+}
+
+function getQuickReply() {
+  return quickReplies[Math.floor(Math.random() * quickReplies.length)]
 }
 
 // Connect to TTS Avatar API
@@ -217,37 +276,60 @@ function connectToAvatarService() {
 }
 
 window.startSession = () => {
-  // Create the <i> element
-  var iconElement = document.createElement("i");
-  iconElement.className = "fa fa-spinner fa-spin";
-  iconElement.id = "loadingIcon"
-  var parentElement = document.getElementById("playVideo");
-  parentElement.prepend(iconElement);
 
-  speechSynthesisConfig.speechSynthesisVoiceName = TTSVoice
-  document.getElementById('playVideo').className = "round-button-hide"
+  if (cogSvcSubKey === '') {
+      alert('Please fill in the subscription key of your speech resource.')
+      return
+  }
+
+  const speechSynthesisConfig = SpeechSDK.SpeechConfig.fromSubscription(cogSvcSubKey, CogSvcRegion)
+  speechSynthesisConfig.endpointId = document.getElementById('customVoiceEndpointId').value
+  speechSynthesisConfig.speechSynthesisVoiceName = document.getElementById('ttsVoice').value
+
+
+  const avatarConfig = new SpeechSDK.AvatarConfig(TalkingAvatarCharacter, TalkingAvatarStyle)
+  avatarConfig.customized = document.getElementById('customizedAvatar').checked
+  avatarSynthesizer = new SpeechSDK.AvatarSynthesizer(speechSynthesisConfig, avatarConfig)
+  avatarSynthesizer.avatarEventReceived = function (s, e) {
+      var offsetMessage = ", offset from session start: " + e.offset / 10000 + "ms."
+      if (e.offset === 0) {
+          offsetMessage = ""
+      }
+
+      console.log("Event received: " + e.description + offsetMessage)
+  }
+
+  const speechRecognitionConfig = SpeechSDK.SpeechConfig.fromSubscription(cogSvcSubKey, cogSvcRegion)
+  speechRecognitionConfig.speechRecognitionLanguage = document.getElementById('sttLocale').value
+  speechRecognizer = new SpeechSDK.SpeechRecognizer(speechRecognitionConfig, SpeechSDK.AudioConfig.fromDefaultMicrophoneInput())
   
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Ocp-Apim-Subscription-Key':'f22920f0f7d64ce39ec6aa9ab6ca06a1',
-      'Content-Type': 'application/json',
-      'Content-Length': '0',},
-  })
-    .then(response => response.text())
-    .then(response => { 
-      speechSynthesisConfig.authorizationToken = response;
-      token = response
-    })
-    .then(() => {
-      speechSynthesizer = new SpeechSDK.SpeechSynthesizer(speechSynthesisConfig, null)
-      requestAnimationFrame(setupWebRTC)
-    })
-  // setupWebRTC()
+  if (azureOpenAIEndpoint === '' || azureOpenAIApiKey === '' || azureOpenAIDeploymentName === '') {
+      alert('Please fill in the Azure OpenAI endpoint, API key and deployment name.')
+      return
+  }
+
+  dataSources = []
+
+  if (azureCogSearchEndpoint === "" || azureCogSearchApiKey === "" || azureCogSearchIndexName === "") {
+    alert('Please fill in the Azure Cognitive Search endpoint, API key and index name.')
+    return
+  } else {
+    setDataSources(azureCogSearchEndpoint, azureCogSearchApiKey, azureCogSearchIndexName)
+  }
+
+  initMessages()
+  
+  if (IceServerUrl === '' || IceServerUsername === '' || IceServerCredential === '') {
+      alert('Please fill in the ICE server URL, username and credential.')
+      return
+  }
+
+  document.getElementById('startSession').disabled = true
+  
+  setupWebRTC(IceServerUrl, IceServerUsername, IceServerCredential)
 }
 
 async function greeting() {
-  
   addToConversationHistory("Hello, my name is Lisa. How can I help you?", "light")
 
   let spokenText = "<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='https://www.w3.org/2001/mstts' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='en-US-JennyNeural'>Hello, my name is Lisa. How can I help you?</voice></speak>"
@@ -267,52 +349,13 @@ async function greeting() {
   })
 }
 
-window.speak = (text) => {
-  async function speak(text) {
-    addToConversationHistory(text, 'dark')
-
-    //fetch("/api/detectLanguage?text="+text, {
-    //  method: "POST"
-    //})
-      //.then(response => response.text())
-      //.then(async language => {
-        //console.log(`Detected language: ${language}`);
-    language = 'ar-AE'
-    const generatedResult = await generateText(text);
-        
-    let spokenTextssml = `<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='https://www.w3.org/2001/mstts' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='en-US-JennyMultilingualNeural'><lang xml:lang="${language}">${generatedResult}</lang></voice></speak>`
-
-    if (language == 'ar-AE') {
-      spokenTextssml = `<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='https://www.w3.org/2001/mstts' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='ar-AE-FatimaNeural'><lang xml:lang="${language}">${generatedResult}</lang></voice></speak>`
-    }
-    let spokenText = generatedResult
-    speechSynthesizer.speakSsmlAsync(spokenTextssml, (result) => {
-      if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
-        console.log("Speech synthesized to speaker for text [ " + spokenText + " ]. Result ID: " + result.resultId)
-      } else {
-        console.log("Unable to speak text. Result ID: " + result.resultId)
-        if (result.reason === SpeechSDK.ResultReason.Canceled) {
-          let cancellationDetails = SpeechSDK.CancellationDetails.fromResult(result)
-          console.log(cancellationDetails.reason)
-          if (cancellationDetails.reason === SpeechSDK.CancellationReason.Error) {
-            console.log(cancellationDetails.errorDetails)
-          }
-        }
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  }
-  speak(text);
-}
 
 window.stopSession = () => {
   speechSynthesizer.close()
 }
 
 window.startRecording = () => {
-  const speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(token, 'westeurope');
+  const speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(token, 'westus2');
   speechConfig.authorizationToken = token;
   speechConfig.SpeechServiceConnection_LanguageIdMode = "Continuous";
   var autoDetectSourceLanguageConfig = SpeechSDK.AutoDetectSourceLanguageConfig.fromLanguages(supported_languages);
@@ -430,36 +473,4 @@ function makeBackgroundTransparent(timestamp) {
   }
 
   window.requestAnimationFrame(makeBackgroundTransparent)
-}
-
-async function getLanguageCode(text) {
-  const endpoint = "https://languagedep.cognitiveservices.azure.com/";
-  const subscription_key = "9be55ef15c3d401e8a2efa6140bde1e0";
-  const apiUrl = 'https://languagedep.cognitiveservices.azure.com/text/analytics/v3.2-preview.1/languages';
-  const requestBody = {'documents': [{'id': '1','text': text}]};
-  fetch(apiUrl,requestBody, {
-    method : 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': subscription_key},
-  })
-    .then(response => response.json)
-    .then(()=>{
-      const language_code = response.documents[0].detectedLanguage.iso6391Name;
-      const language_to_voice = {
-        "de": "de-DE",
-        "en": "en-US",
-        "es": "es-ES",
-        "fr": "fr-FR",
-        "it": "it-IT",
-        "ja": "ja-JP",
-        "ko": "ko-KR",
-        "pt": "pt-BR",
-        "zh_chs": "zh-CN",
-        "zh_cht": "zh-CN",
-        "ar": "ar-AE"
-      };
-      console.log(response)
-      return language_to_voice[language_code]
-    })
 }
